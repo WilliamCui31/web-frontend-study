5dfb647989e235c5dc30cf8bf8215b6e8e8f62df
WilliamCui31 <444738577@qq.com>
2017-08-19 12:21:37 +0800

    delete TodoFlux

--- ToDoMVC/js/app.js
+++
@@ -1,8 +0,0 @@
-var React=require('react');
-var ReactDOM=require('react-dom');
-var TodoApp=require('../js/views/TodoApp');
-
-ReactDOM.render(
-	<TodoApp />,
-	document.querySelector('#todoapp')
-)
